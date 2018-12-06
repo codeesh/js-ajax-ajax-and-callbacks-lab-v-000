17d70674e30394f4185a8cbbe875ba1e6a28df6d
Vinesh Persaud <vineshpersaud@gmail.com>
2018-12-06 16:37:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 $(document).ready(function (){
 });
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  const req = new HMLHttpRequest();
+  req.open('GET','https://api.github.com/search/repositories?q=${searchTerms}`')
+  req.send
+}
